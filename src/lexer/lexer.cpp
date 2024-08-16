adf3cc07af6a9f0a012f203f4de725d0980418de
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2024-08-16 00:19:36 -0500

    Handles whitespace at end of file

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -353,7 +353,9 @@ void koala::lexer::lex() {
         if (lex_operator()) goto lexed;
         if (lex_literal()) goto lexed;
         if (lex_string()) goto lexed;
+        if (iseof(m_c)) goto lexed;
 
+        printf("%d\n", m_c);
         printf("Unknown token at line %u col %u\n",
             m_output.back().loc.row + 1,
             m_output.back().loc.column + 1
